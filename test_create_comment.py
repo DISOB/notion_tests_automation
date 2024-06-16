4175eea1865bd71ea8f7c723b5e4f34ef9b5138a
exanimoff <exanimoff@gmail.com>
2024-06-16 16:48:27 +0300

    test_create_comment stable added

---
+++ test_create_comment.py
@@ -0,0 +1,28 @@
+import pytest
+import allure
+
+from client.common.base_class import ResponseHandler
+from client.comments.create_comment.api import Comments
+from client.comments.create_comment.model import RESPONSE_SCHEMA
+
+@pytest.mark.schemabased
+@allure.feature("comments")
+@allure.severity("allure.severity_level.CRITICAL")
+@allure.description("Verify that creating a comment returns expected results")
+@allure.parent_suite("comments")
+@allure.suite("schemabased")
+def test_create_comment_schemabased(client):
+    comments = Comments(client.NOTION_API_BASE_URL)
+    parent_id = client.PAGE_ID
+    parent_type = "page_id"
+    rich_text = [{
+        "type": "text",
+        "text": {
+            "content": "This is a test comment",
+            "link": None
+        }
+    }]
+    response = comments.create_comment(notion_api_token=client.NOTION_API_TOKEN, parent_id=parent_id, parent_type=parent_type, rich_text=rich_text)
+
+    ResponseHandler.verify_response_code(200, response.status_code)
+    ResponseHandler.verify_json_schema(RESPONSE_SCHEMA, response.json())
