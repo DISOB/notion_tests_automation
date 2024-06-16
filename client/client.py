a974a289ed28031e2369445f8b815db6b2a516c7
exanimoff <exanimoff@gmail.com>
2024-06-16 12:22:10 +0300

    test_get_page stable added

--- client/client.py
+++ client/client.py
@@ -5,6 +5,8 @@ class Client:
     #Сомнительно, но окей
     DATABASE_ID = "c860cb8ca7bf4e7eaba9d22fb9c61a57"
     PARENT_PAGE_ID = "9a6a7be7edd349f588c9f066965d731c"
+    PAGE_ID = "ad4c7a835a9c4b208c62fc6a54953051"
 
     # Maxim_Stoletov
     USER_ID = "4f2bb362-523d-4c44-b903-26d89eef6348"
+
