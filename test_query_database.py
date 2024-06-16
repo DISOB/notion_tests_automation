b4f455750e662bca80d07bef1ff13f77b4dfc021
exanimoff <exanimoff@gmail.com>
2024-06-16 10:58:44 +0300

    query database added

---
+++ test_query_database.py
@@ -0,0 +1,24 @@
+import pytest
+import allure
+from client.databases.query_database.model import QUERY_RESPONSE_SCHEMA
+from client.databases.query_database.api import Databases
+from client.common.base_class import ResponseHandler
+
+@pytest.mark.schemabased
+@allure.feature("databases")
+@allure.severity("allure.severity_level.CRITICAL")
+@allure.description("Verify that querying the database returns expected results")
+@allure.parent_suite("databases")
+@allure.suite("schemabased")
+def test_query_database_schemabased(client):
+    databases = Databases(client.NOTION_API_BASE_URL, client.DATABASE_ID)
+    filter_criteria = {
+        "property": "Status",
+        "status": {
+            "equals": "Done"
+        }
+    }
+    response = databases.query_database(notion_api_token=client.NOTION_API_TOKEN, filter=filter_criteria)
+
+    ResponseHandler.verify_response_code(200, response.status_code)
+    ResponseHandler.verify_json_schema(QUERY_RESPONSE_SCHEMA, response.json())
